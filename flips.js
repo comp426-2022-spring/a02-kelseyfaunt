9c6201d5bcf90a79d09c97c98ad2e11751a3671f
kelseyfaunt <59808596+kelseyfaunt@users.noreply.github.com>
2022-03-16 13:14:44 -0400

    please

--- flips.js
+++ flips.js
@@ -1,6 +1,17 @@
 
 import {coinFlips} from './modules/coin.js';
 //import {createRequire} from 'module';
+
+const args = require('minimist')(process.argv.slice(2));
+
+if(args.length == 0) {
+    console.log(coinFlips(1));
+}
+
+args['number'];
+const number = args.number;
+console.log(coinFlips(number));
+/*
 console.log(process.argv);
 //const require = createRequire(import.meta.url);
 //const idk = require('./modules/coin.mjs');
@@ -17,4 +28,5 @@ temp = args[0].split('=');
 //const number = args.number;
 
 console.log(coinFlips(temp[1]));
+*/
 }
