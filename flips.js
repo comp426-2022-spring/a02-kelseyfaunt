87c68bafae4cee921b4a1940052718fca48b6cab
kelseyfaunt <59808596+kelseyfaunt@users.noreply.github.com>
2022-03-20 14:57:33 -0400

    ugh

--- flips.js
+++ flips.js
@@ -1,5 +1,5 @@
 
-import {coinFlips, countFlips} from './modules/coin.js';
+import {coinFlips, countFlips} from './modules/coin.mjs';
 import {createRequire} from 'module';
 
 const require = createRequire(import.meta.url);
